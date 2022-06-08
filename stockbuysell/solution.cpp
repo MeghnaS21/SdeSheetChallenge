9041686554f52ef49ba5890977c20d8f032307af
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2022-06-08 23:29:34 +0530

    Create solution.cpp

---
+++ stockbuysell/solution.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int maxnum = 0;
+    int minnum = INT_MAX;
+    for (int i = 0; i < prices.size(); i++) {
+      minnum = min(minnum, prices[i]);
+      maxnum = max(maxnum, prices[i] - minnum);
+    }
+  return maxnum;
+}
